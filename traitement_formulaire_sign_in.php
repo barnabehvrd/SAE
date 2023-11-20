a70d3dc56cce38ee3543160908bbc0e0cafe2a03
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-20 16:16:07 +0100

    verif mdp fonctionel

--- traitement_formulaire_sign_in.php
+++ traitement_formulaire_sign_in.php
@@ -1,16 +1,11 @@
 <?php
+/*
 // Connexion à la base de données 
 $utilisateur = "root";
 $serveur = "localhost";
 $motdepasse = "";
 $basededonnees = "sae3";
 
-// Récupération des données du formulaire
-$pwd = $_POST['pwd'];
-$Mail_Uti = $_POST['mail'];
-
-// Création de la session
-session_start();
 
 $connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
 
@@ -28,16 +23,29 @@ $stmt->bind_result($Id_Uti);
 $stmt->fetch();
 $stmt->close();
 $connexion->close();
+*/
+
+// Récupération des données du formulaire
+$pwd = $_POST['pwd'];
+$Mail_Uti = $_POST['mail'];
+
+// Création de la session
+session_start();
 
 $host = 'localhost';
 $dbname = 'sae3';
 $user = 'root';
 $password = '';
 $bdd = new PDO('mysql:host='.$host.';dbname='.$dbname,$user,$password);
+$query = $bdd->query(('SELECT Id_Uti FROM utilisateur WHERE utilisateur.Mail_Uti=\''.$Mail_Uti.'\';'));
+$Id_Uti = $query->fetchAll(PDO::FETCH_ASSOC);
+$Id_Uti=($Id_Uti[0]["Id_Uti"]);
+echo('CALL verifMotDePasse('.$Id_Uti.','. $pwd . ');');
+$query = $bdd->query(('CALL verifMotDePasse(  '.$Id_Uti.','. $pwd . ');'));
+$test = $query->fetchAll(PDO::FETCH_ASSOC);
+var_dump($test[0]);
 
-
-/*
-if ($result == 1) {
+if ($test[0][1] == 1) {
     echo "Le mot de passe correspond.";
     header('Location: index.php');
     $_SESSION['Mail_Uti'] = $Mail_Uti;
@@ -47,7 +55,6 @@ if ($result == 1) {
     echo "Le mot de passe ne correspond pas.";
     header('Location: form_sign_in.php');
 }
-*/
+
 // Fermeture de la connexion
-$connexion->close();
 ?>
