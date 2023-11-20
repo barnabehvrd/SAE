5d537f3df6d4bf277ae13417ff979d0eca43a522
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-20 13:17:33 +0100

    ajout récupération id user

--- traitement_formulaire_sign_in.php
+++ traitement_formulaire_sign_in.php
@@ -17,8 +17,18 @@ $connexion = new mysqli($serveur, $utilisateur, "", $basededonnees);
 session_start();
 
 // Stockage de l'adresse mail dans la session
-$_SESSION['Mail_Uti'] = $Mail_Uti;
+$_SESSION['Mail_Uti'] = $Mail_Uti;$requete = 'SELECT Id_Uti FROM utilisateur WHERE utilisateur.Mail_Uti=?';
+$stmt = $connexion->prepare($requete);
+$stmt->bind_param("s", $_SESSION['Mail_Uti']); // "s" indique que la valeur est une chaîne de caractères
+$stmt->execute();
+// Récupérez le résultat
+$result = $stmt->get_result();
+if ($result->num_rows > 0) {
+    // Fetch the result as an associative array
+    $row = $result->fetch_assoc();
 
+    $_SESSION['Id_Uti'] = $row['Id_Uti'];
+}
 
 // Fermeture de la connexion
 $connexion->close();
