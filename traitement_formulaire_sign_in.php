8e3bc5feb658dad589ae7981a36619483f0d6d64
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-20 15:36:37 +0100

    Update traitement_formulaire_sign_in.php

--- traitement_formulaire_sign_in.php
+++ traitement_formulaire_sign_in.php
@@ -28,44 +28,14 @@ $stmt->bind_result($Id_Uti);
 $stmt->fetch();
 $stmt->close();
 $connexion->close();
-$connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
-$sql = "CALL verifMotDePasse(?, ?)";
-// Vérification de la connexion
-if ($connexion->connect_error) {
-    die("Connection failed: " . $connexion->connect_error);
-}
-var_dump($Id_Uti);
-var_dump($sql);
-var_dump($pwd);
-$stmt = $mysqli->prepare($sql);
-// Appel de la procédure stockée
-$stmt->bind_param("is", $Id_Uti, $pwd); // "is" indique que le premier paramètre est une chaîne de caractères et le deuxième est un entier
-if ($stmt->execute()) {
-    // Récupérer le résultat de la procédure stockée (paramètre de sortie)
-    $stmt->bind_result($parametreSortie);
 
-    // Vérifier si la procédure stockée a retourné des résultats
-    if ($stmt->fetch()) {
-        // Afficher la valeur récupérée
-        echo "La valeur récupérée est : " . $parametreSortie;
-    } else {
-        echo "La procédure stockée n'a retourné aucun résultat.";
-    }
-} else {
-    // Gérer l'erreur d'exécution de la procédure stockée
-    die("L'exécution de la procédure stockée a échoué : " . $stmt->error);
-}
+$host = 'localhost';
+$dbname = 'sae3';
+$user = 'root';
+$password = '';
+$bdd = new PDO('mysql:host='.$host.';dbname='.$dbname,$user,$password);
 
 
-// Récupération du résultat
-// Récupérer le résultat de la procédure stockée (paramètre de sortie)
-$stmt->bind_result($parametreSortie);
-var_dump($parametreSortie);
-// Récupérer la valeur de la sortie
-$stmt->fetch();
-
-$stmt->close();
-// Utilisation du résultat dans votre application
 /*
 if ($result == 1) {
     echo "Le mot de passe correspond.";
