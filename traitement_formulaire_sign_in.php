4228183497ee960d006905c19b56959512baa5b8
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-20 13:53:34 +0100

    Update traitement_formulaire_sign_in.php

--- traitement_formulaire_sign_in.php
+++ traitement_formulaire_sign_in.php
@@ -30,6 +30,28 @@ if ($result->num_rows > 0) {
     $_SESSION['Id_Uti'] = $row['Id_Uti'];
 }
 
+// Préparation de la requête
+$sql = "CALL verifMotDePasse(:mdpAVerifier, :id_Uti, @result)";
+$stmt = $conn->prepare($sql);
+
+// Liaison des paramètres
+$stmt->bindParam(':mdpAVerifier', $pwd, PDO::PARAM_STR);
+$stmt->bindParam(':id_Uti', $Id_Uti, PDO::PARAM_INT);
+
+// Exécution de la requête
+$stmt->execute();
+
+// Récupération du résultat
+$selectResult = $conn->query('SELECT @result as result');
+$result = $selectResult->fetch(PDO::FETCH_ASSOC)['result'];
+
+// Utilisation du résultat dans votre application
+if ($result == 1) {
+    echo "Le mot de passe correspond.";
+} else {
+    echo "Le mot de passe ne correspond pas.";
+}
+
 // Fermeture de la connexion
 $connexion->close();
 header('Location: index.php');
