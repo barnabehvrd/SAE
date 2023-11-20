70bef1b01722928895da200b01bcc25d849858f0
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-20 16:20:15 +0100

    Update traitement_formulaire_sign_in.php

--- traitement_formulaire_sign_in.php
+++ traitement_formulaire_sign_in.php
@@ -1,29 +1,4 @@
 <?php
-/*
-// Connexion à la base de données 
-$utilisateur = "root";
-$serveur = "localhost";
-$motdepasse = "";
-$basededonnees = "sae3";
-
-
-$connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
-
-// Vérification de la connexion
-if ($connexion->connect_error) {
-    die("Connection failed: " . $connexion->connect_error);
-}
-
-// Récupération de l'ID utilisateur
-$requete = 'SELECT Id_Uti FROM utilisateur WHERE utilisateur.Mail_Uti=?';
-$stmt = $connexion->prepare($requete);
-$stmt->bind_param("s", $Mail_Uti); // "s" indique que la valeur est une chaîne de caractères
-$stmt->execute();
-$stmt->bind_result($Id_Uti);
-$stmt->fetch();
-$stmt->close();
-$connexion->close();
-*/
 
 // Récupération des données du formulaire
 $pwd = $_POST['pwd'];
