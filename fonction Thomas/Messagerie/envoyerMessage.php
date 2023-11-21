716ef40cefdaa5f1feb7218e46e6bc655d20ef23
monr4dy <121854761+monr4dy@users.noreply.github.com>
2023-11-21 11:22:10 +0100

    debut css messagerie

--- fonction Thomas/Messagerie/envoyerMessage.php
+++ fonction Thomas/Messagerie/envoyerMessage.php
@@ -12,7 +12,9 @@ function envoyerMessage($id_user, $id_other_people, $content){
 $_SESSION['Id_Uti'] = 2;
 
 if (isset($_SESSION['Id_Uti'], $_GET['Id_Interlocuteur'], $_POST['content'])){
-    envoyerMessage($_SESSION['Id_Uti'], $_GET['Id_Interlocuteur'], $_POST['content']);
+    if ($_POST['content']!=""){
+        envoyerMessage($_SESSION['Id_Uti'], $_GET['Id_Interlocuteur'], $_POST['content']);
+    }
     unset($_POST['content']);
     header("Refresh:0");
 }
--- fonction thomas/Messagerie/afficheMessages.php
+++ fonction thomas/Messagerie/afficheMessages.php
@@ -25,14 +25,17 @@ function afficheMessage($message){
 
 if (isset($_SESSION['Id_Uti'])){
     if (isset($_GET['Id_Interlocuteur'])){
-        
         afficheMessages($_SESSION['Id_Uti'], $_GET['Id_Interlocuteur']);
+        $formDisabled=false;
     }else {
         echo('Veuillez selectionner une conversation.');
+        $formDisabled=true;
     }
 }else{
     echo('Vous n\'êtes pas connecté, vous ne devriez pas avoir accès à cette page.</br>
             Veuillez contacter un administrateur au plus tôt.');
+    $formDisabled=true;
 }
-    
+
+$_SESSION['Id_Uti']=2;
 ?>
