a223206239168acdb78c50f25e9ba14d3ff9e62a
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-23 16:14:00 +0100

    insert commande

--- insert_commande.php
+++ insert_commande.php
@@ -18,16 +18,18 @@
     $bdd=dbConnect();
     $queryNbCommandes = $bdd->query(('SELECT MAX(Id_Commande) FROM COMMANDE;'));
     $returnqueryNbCommandes = $queryNbCommandes->fetchAll(PDO::FETCH_ASSOC);
-    $nbCommandes = $returnqueryNbCommandes[0]["MAX(Id_Commande)"];
+    $nbCommandes = $returnqueryNbCommandes[0]["MAX(Id_Commande)"]+1;
     $insertionCommande = "INSERT INTO COMMANDE (Id_Commande, Id_Statut, Id_Prod, Id_Uti) VALUES ($nbCommandes, 1, $Id_Prod, $Id_Uti);";
-    echo $nbCommandes;
+    echo $insertionCommande;
+
+    $bdd->query($insertionCommande);
+
     foreach ($Url as $produit => $quantite) {
-
-        
-        
-        
+        $insertionProduit = "INSERT INTO CONTENU (Id_Commande, Id_Produit, Qte_Produit_Commande, Num_Produit_Commande) VALUES ($nbCommandes, $produit, $quantite, $i);";
+        echo $insertionProduit;
+        $bdd->query($insertionProduit);
+        $i++;
     }
-
-
-//header('Location: commandes.php?Id_Prod=314');
+    
+    header('Location: commandes.php?Id_Prod=314');
 ?>
