80986f85d3f162c55bc7cf03b51fb2c0e7680cfc
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-24 20:01:15 +0100

    commit avant dodo

--- bug_report.php
+++ bug_report.php
@@ -1,28 +1,26 @@
 <?php
 
 // Connexion à la base de données (remplacez ces valeurs par les vôtres)
-$utilisateur = "root";
-$serveur = "localhost";
-//$motdepasse = "root";
-$basededonnees = "sae3";
-
-// Création de la connexion
-$connexion = new mysqli($serveur, $utilisateur, "", $basededonnees);
-
-// Vérification de la connexion
-if ($connexion->connect_error) {
-  die("Erreur de connexion : " . $connexion->connect_error);
-}
 
 // Récupération des données du formulaire
-$mail = $_POST['mail'];
+
+session_start();
+// Database connection
+$utilisateur = "inf2pj02";
+$serveur = "localhost";
+$motdepasse = "ahV4saerae";
+$basededonnees = "inf2pj_02";
+// Connect to database
+$bdd = new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse);
 $message = $_POST['message'];
-// Fermeture de la connexion
-
 // Appel de la fonction SQL
-$connexion->query("CALL broadcast_admin('{$mail}', '{$message}')");
-
-$connexion->close();
+if (isset($_SESSION["Id_Uti"]) && isset($message)) {
+  // Les variables de session sont définies, exécuter la requête
+  $bdd->query('CALL broadcast_admin(' . $_SESSION["Id_Uti"] . ', \'' . $message . '\');');
+} else {
+  // Une ou plusieurs variables ne sont pas définies, afficher un message d'erreur ou prendre une autre action
+  echo "Erreur : Les variables de session ne sont pas définies.";
+}
 
 // Redirection vers la page d'accueil
 header('Location: index.php');
--- index.php
+++ index.php
@@ -134,8 +134,6 @@
 			</div>
 			<form class="formulaire" action="bug_report.php" method="post">
 					<p class= "centered">report a bug</p>
-					<label for="mail">mail :</label>
-					<input type="text" name="mail" id="mail" required><br><br>
 					<label for="pwd">message : </label>
 					<input type="text" name="message" id="message" required><br><br>
 					<input type="submit" value="Envoyer">
