cbd616f84e839efa8f73f9c5df73af559c8017bb
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-27 17:48:15 +0100

    test encore

--- modify_product.php
+++ modify_product.php
@@ -1,15 +1,25 @@
 <?php
-//var_dump($_POST);
-$Id_Produit=$_POST["IdProductAmodifier"];
-$Nom_Produit=$_POST["nomProduit"];
-$Categorie=$_POST["categorie"];
-$Prix=$_POST["prix"];
-$Prix_Unite=$_POST["unitPrix"];
-$Quantite=$_POST["quantite"];
-$Quantite_Unite=$_POST["unitQuantite"];
+    function dbConnect(){
+        $utilisateur = "inf2pj02";
+        $serveur = "localhost";
+        $motdepasse = "ahV4saerae";
+        $basededonnees = "inf2pj_02";
+        // Connect to database
+        return new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse);
+    }
+    $bdd=dbConnect();
+    //var_dump($_POST);
 
-$updateProduit= "UPDATE PRODUIT SET Nom_Produit = ".$Nom_Produit.", Id_Type_Produit = ".$Categorie.", Qte_Produit = ".$Quantite.", Id_Unite_Stock = ".$Quantite_Unite.", Prix_Produit_Unitaire = ".$Prix.", Id_unite_Prix = ".$Prix_Unite." WHERE Id_Produit = ".$Id_Produit .";";
-$bdd->exec($updateProduit);
+    $Id_Produit=$_POST["IdProductAmodifier"];
+    $Nom_Produit=$_POST["nomProduit"];
+    $Categorie=$_POST["categorie"];
+    $Prix=$_POST["prix"];
+    $Prix_Unite=$_POST["unitPrix"];
+    $Quantite=$_POST["quantite"];
+    $Quantite_Unite=$_POST["unitQuantite"];
 
-//header('Location: product_modification.php');
+    $updateProduit= "UPDATE PRODUIT SET Nom_Produit = ".$Nom_Produit.", Id_Type_Produit = ".$Categorie.", Qte_Produit = ".$Quantite.", Id_Unite_Stock = ".$Quantite_Unite.", Prix_Produit_Unitaire = ".$Prix.", Id_unite_Prix = ".$Prix_Unite." WHERE Id_Produit = ".$Id_Produit .";";
+    $bdd->exec($updateProduit);
+
+    //header('Location: product_modification.php');
 ?>
