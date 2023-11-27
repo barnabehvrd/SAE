e8819da44e2472f7f943f683ce087afa0f743246
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-27 18:48:47 +0100

    ajout file

---
+++ insert_product_picture.php
@@ -0,0 +1,4 @@
+<form action="upload_product.php" method="post" enctype="multipart/form-data">
+<input type="file" name="image" accept=".png" required>
+<button type="submit">Envoyer</button>
+</form>
