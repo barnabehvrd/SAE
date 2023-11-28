d7cb539fac0399df8fddd2654c116e2dc5d15080
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-28 17:23:04 +0100

    update

--- traitement_code.php
+++ traitement_code.php
@@ -3,16 +3,18 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $enteredCode = $_POST["code"];
-    $expectedCode = isset($_SESSION["code"]);
+    $expectedCode = $_SESSION["code"];
 
     // Comparer le code saisi avec le code attendu
-    if ($enteredCode === $expectedCode) {
+    if ($enteredCode == $expectedCode) {
         // Code correct, rediriger vers form_update.php
-        header("Location: form_update.php");
+        echo("code correct");
+        //header("Location: form_update_pwd.php");
         exit();
     } else {
         // Code incorrect, rediriger vers form_code.php
-        header("Location: form_code.php?result=failure1");
+        echo("code incoreect");
+        //header("Location: form_code.php?result=failure1");
         exit();
     }
 }
