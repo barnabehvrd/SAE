9dd02146850a8882f18618fa1bbc87ddec3a59bf
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-28 13:26:08 +0100

    amelioration img choix insertion

--- insert_product_picture.php
+++ insert_product_picture.php
@@ -1,4 +1,7 @@
+<h2>Ajouter une image</h2>
 <form action="upload_product.php" method="post" enctype="multipart/form-data">
 <input type="file" name="image" accept=".png" required>
 <button type="submit">Envoyer</button>
+<br>
+<button class="button"><a href="mes_produits.php">Ignorer</a></button>  
 </form>
