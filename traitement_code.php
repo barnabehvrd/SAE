e3582da0307ed3084f244d9b528c770c43e45915
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-28 17:54:05 +0100

    update ca fonctionne

--- traitement_code.php
+++ traitement_code.php
@@ -10,12 +10,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($enteredCode == $expectedCode) {
         // Code correct, rediriger vers form_update.php
         echo("code correct");
-        //header("Location: form_update_pwd.php");
+        header("Location: form_update_pwd.php");
         exit();
     } else {
         // Code incorrect, rediriger vers form_code.php
         echo("code incoreect");
-        //header("Location: form_code.php?result=failure1");
+        header("Location: form_code.php?result=failure1");
         exit();
     }
 }
