b862de18cad4f1d80037eeaf5437568da689e042
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 10:58:13 +0100

    Update download_pdf.php

--- download_pdf.php
+++ download_pdf.php
@@ -1,4 +1,14 @@
 <?php
+    	function dbConnect(){
+            $host = 'localhost';
+            $dbname = 'inf2pj_02';
+            $user = 'inf2pj02';
+            $password = 'ahV4saerae';
+            return new PDO('mysql:host='.$host.';dbname='.$dbname,$user,$password);
+      }
+
+	  $bdd=dbConnect();
+	  session_start();
 // Récupération des données de la commande
 $Id_Commande = $_POST["idCommande"];
 $queryGetCommande = $bdd->query(('SELECT Desc_Statut, Id_Commande, COMMANDE.Id_Uti, UTILISATEUR.Nom_Uti, UTILISATEUR.Prenom_Uti, COMMANDE.Id_Statut FROM COMMANDE INNER JOIN info_producteur ON COMMANDE.Id_Prod=info_producteur.Id_Prod INNER JOIN STATUT ON COMMANDE.Id_Statut=STATUT.Id_Statut INNER JOIN UTILISATEUR ON COMMANDE.Id_Uti=UTILISATEUR.Id_Uti WHERE COMMANDE.Id_Commande=' . $Id_Commande . ';'));
