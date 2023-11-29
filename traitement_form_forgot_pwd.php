552f95fc1d0da608c3ea32493df55a289f7d57ed
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2023-11-29 11:52:40 +0100

    sans le spam

--- traitement_form_forgot_pwd.php
+++ traitement_form_forgot_pwd.php
@@ -2,24 +2,19 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_start();
     $email = $_POST["email"];
-    $_SESSION["Mail_Uti"] = $email;
+    $_SESSION["Mail_Uti"]=$email;
+    // Génération d'un code aléatoire à 6 chiffres
+    $code = rand(100000, 999999);
+    $_SESSION["code"]=$code;
+    // Envoi du code par e-mail
+    $subject = "Réinitialisation de mot de passe";
+    $message = "Votre code de réinitialisation de mot de passe est : $code";
+    $headers = "From: no-reply@letalenligne.com"; // Remplacez par votre adresse e-mail
 
-    // Effectuez l'envoi du code 10 fois
-    for ($i = 0; $i < 100; $i++) {
-        // Génération d'un code aléatoire à 6 chiffres
-        $code = rand(100000, 999999);
-        $_SESSION["code"] = $code;
+    // Envoi de l'e-mail
+    $mailSent = mail($email, $subject, $message, $headers);
 
-        // Envoi du code par e-mail
-        $subject = "Réinitialisation de mot de passe";
-        $message = "Votre code de réinitialisation de mot de passe est : $code";
-        $headers = "From: no-reply@letalenligne.com"; // Remplacez par votre adresse e-mail
-
-        // Envoi de l'e-mail
-        $mailSent = mail($email, $subject, $message, $headers);
-
-        // Si l'envoi échoue, vous pouvez prendre des mesures appropriées ici
-    }
+    // Redirection avec le résultat de l'envoi
     $redirectUrl = 'form_code.php';
     if ($mailSent) {
         $redirectUrl .= '?result=success';
