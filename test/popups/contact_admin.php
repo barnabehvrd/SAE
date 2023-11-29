4318215fb5cabd85f5608749163ae292414f508e
monr4dy <121854761+monr4dy@users.noreply.github.com>
2023-11-29 17:47:59 +0100

    a

--- test/popups/contact_admin.php
+++ test/popups/contact_admin.php
@@ -6,16 +6,15 @@
 		</form>
         <p class="titrePopup">Contacter un administrateur ou report un bug</p>
         <form class="formPopup" method="post">
-            <div>
+            <?php if(!isset($_SESSION['Mail_Uti'])){ ?>
+            <div >
                 <label for="mail">Mail :</label>
-                <input type="hidden" value='0' name="formClicked">
-                <input class="zoneDeTextePopup" type="text" pattern="[A-Za-z0-9._-]{1,20}@[A-Za-z0-9.-]{1,16}\.[A-Za-z]{1,4}" name="mail" id="mail" required
-                <?php if(isset($_SESSION['Mail_Uti'])){
-                    echo 'disable';} ?> >
+                <input class="zoneDeTextePopup" type="text" pattern="[A-Za-z0-9._-]{1,20}@[A-Za-z0-9.-]{1,16}\.[A-Za-z]{1,4}" name="mail" id="mail" required>
             </div>
+            <?php } ?>
             <label for="pwd">Message : </label>
             <textarea class="grosseZoneDeTextePopup" name="message" pattern="[A-Za-z0-9.]{1,4096}" required></textarea>
-            <input class="boutonPopup" type="submit" value="Envoyer">
+            <input class="boutonPopup" type="submit" value="Envoyer" name="formClicked">
         </form>
         <?php
         if (isset($_POST['formClicked'])){
---
+++ test/traitements/chargement_info_perso.php
@@ -0,0 +1,18 @@
+<?php
+$utilisateur = "inf2pj02";
+$serveur = "localhost";
+$motdepasse = "ahV4saerae";
+$basededonnees = "inf2pj_02";
+$connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
+
+// Vérifiez la connexion
+if ($connexion->connect_error) {
+    die("Erreur de connexion : " . $connexion->connect_error);  
+}
+// Préparez la requête SQL en utilisant des requêtes préparées pour des raisons de sécurité
+$requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=?';
+$stmt = $connexion->prepare($requete);
+$stmt->bind_param("s", $_SESSION['Mail_Uti']); // "s" indique que la valeur est une chaîne de caractères
+$stmt->execute();
+$result = $stmt->get_result();
+?>
