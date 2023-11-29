eac14be68396610b9b95f17c2e91b07935eba7fd
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 11:45:26 +0100

    merde

--- download_pdf.php
+++ download_pdf.php
@@ -1,21 +1,4 @@
 <?php
-function dbConnect(){
-    $host = 'localhost';
-    $dbname = 'inf2pj_02';
-    $user = 'inf2pj02';
-    $password = 'ahV4saerae';
-    return new PDO('mysql:host='.$host.';dbname='.$dbname,$user,$password);
-}
-
-$bdd=dbConnect();
-session_start();
-// Récupération des données de la commande
-$Id_Commande = $_POST["idCommande"];
-$queryGetCommande = $bdd->query(('SELECT Desc_Statut, Id_Commande, COMMANDE.Id_Uti, UTILISATEUR.Nom_Uti, UTILISATEUR.Prenom_Uti, COMMANDE.Id_Statut FROM COMMANDE INNER JOIN info_producteur ON COMMANDE.Id_Prod=info_producteur.Id_Prod INNER JOIN STATUT ON COMMANDE.Id_Statut=STATUT.Id_Statut INNER JOIN UTILISATEUR ON COMMANDE.Id_Uti=UTILISATEUR.Id_Uti WHERE COMMANDE.Id_Commande=' . $Id_Commande . ';'));
-$returnQueryGetCommande = $queryGetCommande->fetchAll(PDO::FETCH_ASSOC);
-$Id_Commande = $returnQueryGetCommande[0]["Id_Commande"];
-$Desc_Statut = $returnQueryGetCommande[0]["Desc_Statut"];
-
 require('fpdf/fpdf.php');
 
 // Créer une instance de la classe FPDF
@@ -32,6 +15,9 @@ $pdf->Cell(40, 10, 'Bonjour, voici votre PDF!');
 
 // Nom du fichier PDF à télécharger
 $nom_fichier = 'votre_fichier.pdf';
+
+// Nettoyer le tampon de sortie
+ob_clean();
 
 // Envoi des en-têtes HTTP pour forcer le téléchargement du fichier
 header('Content-Type: application/pdf');
