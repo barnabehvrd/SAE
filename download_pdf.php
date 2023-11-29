0bdc476822898a3c1299034d0cb2b4e4d9bd5224
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 14:36:01 +0100

    corr

--- download_pdf.php
+++ download_pdf.php
@@ -79,7 +79,7 @@ $pdf->Ln(5); // Sauts de ligne réduits
 // Informations sur la commande
 $pdf->Cell(0, 5, 'COMMANDE n°'.$Id_Commande.' :', 0, 1);
 
-// Tableau avec des produits générés aléatoirement
+
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(40, 8, 'Produit', 1);
 $pdf->Cell(40, 8, 'Prix Unitaire', 1);
@@ -95,12 +95,7 @@ $iterateurProduit=0;
 $nbProduit=count($returnQueryGetProduitCommande);
 
 
-// Génération de produits aléatoires (exemple avec 3 produits)
-$produits = [
-    ['Produit 1', 10, 2],
-    ['Produit 2', 15, 3],
-    ['Produit 3', 20, 1],
-];
+$produits = [];
 
 while ($iterateurProduit<$nbProduit){
     $Nom_Produit=$returnQueryGetProduitCommande[$iterateurProduit]["Nom_Produit"];
@@ -117,11 +112,12 @@ while ($iterateurProduit<$nbProduit){
 $pdf->SetFont('Arial', '', 12);
 foreach ($produits as $produit) {
     $pdf->Cell(40, 8, $produit[0], 1);
-    $pdf->Cell(40, 8, '$' . $produit[1], 2, 1);
+    $pdf->Cell(40, 8, '$' . $produit[1], 1); 
     $pdf->Cell(30, 8, $produit[2], 1);
-    $pdf->Cell(40, 8, '$' . intval($Prix_Produit_Unitaire)*intval($Qte_Produit_Commande), 1);
+    $pdf->Cell(40, 8, '$' . intval($produit[1]) * intval($produit[2]), 1); 
     $pdf->Ln();
 }
+
 
 $pdf->Ln(5); // Saut de ligne réduit
 
