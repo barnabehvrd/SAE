2f1589daad3e2db27cab53df7ed5aab8d80d5cde
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 10:26:24 +0100

    test encore

--- download_pdf.php
+++ download_pdf.php
@@ -6,8 +6,11 @@ header('Content-Type: application/pdf');
 header('Content-Disposition: inline; filename=output.pdf');
 
 // Générer le contenu PDF en utilisant des balises HTML
-echo '<html>
+echo '<!DOCTYPE html>
+        <html lang="en">
         <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Mon PDF</title>
         </head>
         <body>
