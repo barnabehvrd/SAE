379525f04fa02f638dd479ab4f240b36c1fa2c78
monr4dy <121854761+monr4dy@users.noreply.github.com>
2023-11-29 18:54:19 +0100

    derniers petits trucs de la fin de journée (pour moi) PS: la deconnexion ne marche pas PPS:c'est thomas

--- test/popups/info_perso.php
+++ test/popups/info_perso.php
@@ -1,9 +1,13 @@
 <div class="popup">
     <div class="contenuPopup">
-        <div>
+        <div style="display:flex;justify-content:space-between;">
             <form method="post">
 				<input type="submit" value="" class="boutonQuitPopup">
                 <input type="hidden" name="popup" value="">
+		    </form>
+            <form method="post">
+				<input type="submit" value="se deconnecter" name="deconnexion">
+                <input type="hidden" name="deconnexion">
 		    </form>
         </div>
         <p class="titrePopup">Informations personelles</p>
@@ -15,17 +19,17 @@
                     <form method="post">
                         
                     <!--  Set default values to current user information -->
-                    <label for="new_nom">Nom :</label><br>
-                    <input type="text" name="new_nom" value=<?php echo ($row["Nom_Uti"]) ?>><br>
+                    <label for="new_nom">Nom :</label>
+                    <input type="text" name="new_nom" value=<?php echo ($row["Nom_Uti"]) ?>>
 
-                    <label for="new_prenom">Prénom :</label><br>
-                    <input type="text" name="new_prenom" value=<?php echo ($row["Prenom_Uti"]) ?>><br>
+                    <label for="new_prenom">Prénom :</label>
+                    <input type="text" name="new_prenom" value=<?php echo ($row["Prenom_Uti"]) ?>>
                     
-                    <label for="new_adr">Adresse postale :</label><br>
-                    <input type="text" name="new_adr" value="<?php echo ($row["Adr_Uti"])?>"><br>
+                    <label for="new_adr">Adresse postale :</label>
+                    <input type="text" name="new_adr" value="<?php echo ($row["Adr_Uti"])?>">
                     
                     <!-- Add the submit button -->
-                    <input type="submit" value="Modifier">
+                    <input type="submit" name="formClicked" value="Modifier">
                     </form>
                     <?php
                     //var_dump($row["Adr_Uti"]);
@@ -33,8 +37,6 @@
             } else {
                 ?><p>Aucun résultat trouvé pour votre compte, veuillez contacter le support.</p><?php
             }
-            $stmt->close();
-            $connexion->close();
             ?>
         </div>
         <?php
@@ -42,53 +44,10 @@
             require 'traitements/update_user_info.php';
             unset($_POST['formClicked']);
         }
+        if(isset($_POST['deconnexion'])){
+            require 'traitements/log_out.php';
+            unset($_POST['deconnexion']);
+        }
         ?>
     </div>
 </div>
-
-
-<!--
-            
-                <button class="button"><a href="log_out.php">déconexion</a></button>
-            
-
-                    
-                                
-                        </div>
-                </div>
-                <div class="square">
-                                   <form action="update_pwd_info.php" method="post">
-                                        <label for="new_mdp1">nouveau mot de passe :</label><br>
-                                        <input type="text" name="pwd1"> <br>
-                                        <label for="new_mdp2">ressaisissez le nouveau mot de passe :</label><br>
-                                        <input type="text" name="pwd2"> <br>
-                                        <input type="submit" value="Modifier">
-                                   </form>
-                                        
-                </div>
-                <div class="square">
-                                     <label for="btn-producteur">Je souhaite supprimer mon compte</label>
-				                        <input type="button" onclick="window.location.href='del_acc.php'" id="del_acc_button" value="supprimer">
-				
-                </div>
-                <div class="square">
-                    <h2>modifier votre photo de profil</h2>
-                    <form action="upload.php" method="post" enctype="multipart/form-data">
-                    <input type="file" name="image" accept=".png" required>
-                    <button type="submit">Envoyer</button>
-                    </form>
-                </div>
-            </div>
-                <form class="formulaire" action="bug_report.php" method="post">
-					<p class= "centered">report a bug</p>
-					<label for="pwd">message : </label>
-					<input type="text" name="message" id="message" required><br><br>
-					<input type="submit" value="Envoyer">
-			    </form>
-			</div>
-
-			
-		</div>
-    </div>
-</body>
-</html> -->
