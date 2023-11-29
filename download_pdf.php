922ab5d8024d04b51f2919fbb7b828fda2686cd3
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 13:18:28 +0100

    ameliaation prod pdf

--- download_pdf.php
+++ download_pdf.php
@@ -2,13 +2,42 @@
 // Inclure la bibliothèque FPDF
 require('fpdf/fpdf.php');
 
-// Créer une instance de FPDF
-$pdf = new FPDF();
-$pdf->AddPage();
-$pdf->SetFont('Arial', 'B', 16);
-$pdf->Cell(40, 10, 'Bonjour, ceci est un exemple de PDF!');
+class MonPDF extends FPDF
+{
+    // En-tête
+    function Header()
+    {
+        // Titre
+        $this->SetFont('Arial', 'B', 12);
+        $this->Cell(0, 10, 'Mon Document', 0, 1, 'C');
 
-// Enregistrez le PDF dans un fichier temporaire
+        // Ligne de séparation
+        $this->Cell(0, 0, '', 'T');
+        $this->Ln(10); // Saut de ligne
+    }
+
+    // Pied de page
+    function Footer()
+    {
+        // Numéro de page
+        $this->SetY(-15);
+        $this->SetFont('Arial', 'I', 8);
+        $this->Cell(0, 10, 'Page ' . $this->PageNo(), 0, 0, 'C');
+    }
+}
+
+// Créer une instance de MonPDF
+$pdf = new MonPDF();
+$pdf->AddPage();
+
+// Ajouter les valeurs
+$pdf->SetFont('Arial', '', 12);
+
+$pdf->Cell(0, 10, 'Producteur', 0, 1);
+$pdf->Cell(0, 10, 'PROFESSION', 0, 1);
+$pdf->Cell(0, 10, 'ADRESSE', 0, 1);
+
+// Enregistrer le PDF dans un fichier temporaire
 $nom_fichier = tempnam(sys_get_temp_dir(), 'pdf');
 $pdf->Output($nom_fichier, 'F');
 
