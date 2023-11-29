3afe0a3843bc401b79b797c2dc98ced8043ef986
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 11:05:08 +0100

    Update download_pdf.php

--- download_pdf.php
+++ download_pdf.php
@@ -17,85 +17,8 @@ $Id_Commande = $returnQueryGetCommande[0]["Id_Commande"];
 $Desc_Statut = $returnQueryGetCommande[0]["Desc_Statut"];
 $Nom_Producteur = $returnQueryGetCommande[0]["Nom_Prod"];
 
-// Génération du contenu PDF
-$pdfContent = '%PDF-1.3
-1 0 obj
-<< /Type /Catalog
-/Pages 2 0 R >>
-endobj
-2 0 obj
-<< /Type /Pages
-/Kids [3 0 R]
-/Count 1 >>
-endobj
-3 0 obj
-<< /Type /Page
-/Parent 2 0 R
-/MediaBox [0 0 612 792]
-/Resources << /Font << /F1 << /Type /Font /Subtype /Type1 /BaseFont /Helvetica-Bold >> >> >>
-/Contents 4 0 R >>
-endobj
-4 0 obj
-<< /Length 300 >>
-stream
-BT
-/F1 18 Tf
-50 750 Td
-(Bon de Commande) Tj
-/F1 12 Tf
-50 730 Td
-(--------------------------------------------) Tj
-50 710 Td
-(Commande ID: ' . $Id_Commande . ') Tj
-50 690 Td
-(Statut: ' . $Desc_Statut . ') Tj
-50 670 Td
-(Producteur: ' . $Nom_Producteur . ') Tj
-50 650 Td
-(--------------------------------------------) Tj
-50 630 Td
-(Nombre de produits: 3) Tj
-50 610 Td
-(Produit 1: T-shirt) Tj
-50 590 Td
-(Quantité: 5) Tj
-50 570 Td
-(Prix unitaire: $10) Tj
-50 550 Td
-(--------------------------------------------) Tj
-50 530 Td
-(Produit 2: Tomates) Tj
-50 510 Td
-(Quantité: 3) Tj
-50 490 Td
-(Prix unitaire: $5) Tj
-50 470 Td
-(--------------------------------------------) Tj
-50 450 Td
-(Produit 3: Chaussures) Tj
-50 430 Td
-(Quantité: 2) Tj
-50 410 Td
-(Prix unitaire: $50) Tj
-50 390 Td
-(--------------------------------------------) Tj
-ET
-endstream
-endobj
-xref
-0 5
-0000000000 65535 f 
-0000000018 00000 n 
-0000000077 00000 n 
-0000000178 00000 n 
-0000000653 00000 n 
-trailer
-<< /Root 1 0 R
-/Size 5 >>
-startxref
-622
-%%EOF
-';
+// Génération du contenu PDF en binaire
+$pdfContent = pack("C*", 37, 80, 68, 70, 45, 49, 46, 51, 10, 49, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10, 47, 84, 121, 112, 101, 32, 47, 67, 97, 116, 97, 108, 111, 103, 10, 47, 80, 97, 103, 101, 115, 32, 50, 32, 48, 32, 82, 10, 47, 67, 111, 117, 110, 116, 32, 49, 10, 47, 75, 105, 100, 115, 32, 91, 51, 32, 48, 32, 82, 93, 10, 47, 75, 105, 100, 115, 32, 91, 51, 32, 48, 32, 82, 93, 10, 47, 67, 111, 117, 110, 116, 32, 49, 10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10, 50, 32, 48, 32, 111, 98, 106, 10, 60, 60, 10, 47, 84, 121, 112, 101, 32, 47, 80, 97, 103, 101, 115, 10, 47, 75, 105, 100, 115, 32, 91, 52, 32, 48, 32, 82, 93, 10, 47, 67, 111, 117, 110, 116, 32, 49, 10, 62, 62, 10, 101, 110, 100, 111, 98, 106, 10, 120, 114, 101, 102, 10, 37, 37, 69, 79, 70, 10);
 
 // Envoie les entêtes pour indiquer que le contenu est un PDF à télécharger
 header('Content-Type: application/pdf');
