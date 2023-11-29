15bc964a09a0a6d0389772125215c9b090f5264c
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 18:50:47 +0100

    0

--- delivery.php
+++ delivery.php
@@ -22,6 +22,9 @@
       if (isset($_POST["typeCategorie"])==true){
         $filtreCategorie=$_POST["typeCategorie"];
       }
+      else{
+        $filtreCategorie=0;
+      }
     ?>
     <div class="container">
         <div class="left-column">
