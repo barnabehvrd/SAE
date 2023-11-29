95dbfb619e2b5c9bbf02e367e5e75309875c6786
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-11-29 10:23:38 +0100

    pdf v363654484897

--- download_pdf.php
+++ download_pdf.php
@@ -1,19 +1,18 @@
 <?php
-// Définir le type de contenu pour PDF
+// Définir le type de contenu comme PDF
 header('Content-Type: application/pdf');
 
-// Spécifier le nom du fichier PDF
-header('Content-Disposition: attachment; filename="exemple.pdf"');
+// Indiquer que le contenu sera téléchargé en tant que fichier PDF avec le nom 'output.pdf'
+header('Content-Disposition: inline; filename=output.pdf');
 
-// Générer le contenu PDF
-echo '<!DOCTYPE html>
-<html>
-<head>
-    <title>Exemple PDF</title>
-</head>
-<body>
-    <h1>Contenu du PDF</h1>
-    <p>Ceci est un exemple de fichier PDF généré en utilisant PHP sans bibliothèque externe.</p>
-</body>
-</html>';
+// Générer le contenu PDF en utilisant des balises HTML
+echo '<html>
+        <head>
+            <title>Mon PDF</title>
+        </head>
+        <body>
+            <h1>Mon PDF</h1>
+            <p>Ceci est le contenu du PDF généré depuis PHP.</p>
+        </body>
+      </html>';
 ?>
