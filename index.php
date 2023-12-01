232be2bd88e46bf56775b411ef82ed1af8dcefec
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-12-01 15:46:04 +0100

    ee

--- index.php
+++ index.php
@@ -171,26 +171,42 @@
 
 
 
-$url='http://nominatim.openstreetmap.org/search?q=1645,%20route%20des%20Lucioles,%20Biot&format=json';
-$ch=curl_init($url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
-$response = curl_exec($ch);
-var_dump($response);
-                                // Analyser la réponse JSON
-                                $data = json_decode($response);
-                                // Vérifier si la réponse a été correctement analysée
-                                if (!empty($data) && is_array($data) && isset($data[0])) {
-                                    // Récupérer la latitude et la longitude
-                                    $latitude = $data[0]->lat;
-                                    $longitude = $data[0]->lon;
-                                    // Afficher les résultats
-                                    echo "Latitude : $latitude<br>";
-                                    echo "Longitude : $longitude";
-                                } else {
-                                    // En cas d'erreur ou si aucune correspondance n'est trouvée, afficher un message
-                                    echo "Erreur lors de l'extraction des données de géocodage.";
-                                }
 
+                                $url = 'http://nominatim.openstreetmap.org/search?q=1645,%20route%20des%20Lucioles,%20Biot&format=json';
+
+                                $ch = curl_init($url);
+                                curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                                curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Permet de suivre les redirections
+                                
+                                $response = curl_exec($ch);
+                                
+                                // Vérifier s'il y a eu une erreur cURL
+                                if (curl_errno($ch)) {
+                                    echo 'Erreur cURL : ' . curl_error($ch);
+                                } else {
+                                    // Analyser la réponse JSON
+                                    $data = json_decode($response);
+                                
+                                    // Vérifier si la réponse a été correctement analysée
+                                    if (!empty($data) && is_array($data) && isset($data[0])) {
+                                        // Récupérer la latitude et la longitude
+                                        $latitude = $data[0]->lat;
+                                        $longitude = $data[0]->lon;
+                                        
+                                        // Afficher les résultats
+                                        echo "Latitude : $latitude<br>";
+                                        echo "Longitude : $longitude";
+                                    } else {
+                                        // En cas d'erreur ou si aucune correspondance n'est trouvée, afficher un message
+                                        echo "Erreur lors de l'extraction des données de géocodage.";
+                                    }
+                                }
+                                
+
+
+                                
+                                // Fermer la ressource cURL
+                                curl_close($ch);
 
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_assoc()) {
