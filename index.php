18b9ffc8efc552fa4f878eba8d52c18383df712a
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-12-01 17:34:29 +0100

    ||

--- index.php
+++ index.php
@@ -151,7 +151,7 @@
             <script>
                 function AfficheRange2(newVal) {
                     var monCurseurKm = document.getElementById("monCurseurKm");
-                    if (newVal >= 100) {
+                    if ((newVal >= 100) || ($rayon=>100)) {
                         monCurseurKm.innerHTML = "Rayon de " + newVal + "+ ";
                     } else {
                         monCurseurKm.innerHTML = "Rayon de " + newVal + " ";
@@ -239,18 +239,28 @@
                                 $longitudeUti=$coordonneesUti[1];
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_assoc()) {
-                                        $urlProd = 'https://nominatim.openstreetmap.org/search?format=json&q=' . urlencode($row["Adr_Uti"]);
-                                        $coordonneesProd=latLongGps($urlProd);
-                                        $latitudeProd=$coordonneesProd[0];
-                                        $longitudeProd=$coordonneesProd[1];
-                                        $distance=distance($latitudeUti, $longitudeUti, $latitudeProd, $longitudeProd);
-                                        if (($rayon>=100)or ($distance<$rayon)){
+                                        if ($rayon>=100){
                                             echo '<a href="producteur.php?Id_Prod='. $row["Id_Uti"] . '" class="square"  >';
                                             echo "Nom : " . $row["Nom_Uti"] . "<br>";
                                             echo "Prénom : " . $row["Prenom_Uti"]. "<br>";
                                             echo "Adresse : " . $row["Adr_Uti"] . "<br>";
                                             echo '<img src="/~inf2pj02/img_producteur/' . $row["Id_Prod"]  . '.png" alt="Image utilisateur" style="width: 100%; height: 85%;" ><br>';
                                             echo '</a> ';  
+                                        }    
+                                        else{
+                                            $urlProd = 'https://nominatim.openstreetmap.org/search?format=json&q=' . urlencode($row["Adr_Uti"]);
+                                            $coordonneesProd=latLongGps($urlProd);
+                                            $latitudeProd=$coordonneesProd[0];
+                                            $longitudeProd=$coordonneesProd[1];
+                                            $distance=distance($latitudeUti, $longitudeUti, $latitudeProd, $longitudeProd);
+                                            if ($distance<$rayon){
+                                                echo '<a href="producteur.php?Id_Prod='. $row["Id_Uti"] . '" class="square"  >';
+                                                echo "Nom : " . $row["Nom_Uti"] . "<br>";
+                                                echo "Prénom : " . $row["Prenom_Uti"]. "<br>";
+                                                echo "Adresse : " . $row["Adr_Uti"] . "<br>";
+                                                echo '<img src="/~inf2pj02/img_producteur/' . $row["Id_Prod"]  . '.png" alt="Image utilisateur" style="width: 100%; height: 85%;" ><br>';
+                                                echo '</a> ';  
+                                            }    
                                         }
                                     }
                                 } else {
