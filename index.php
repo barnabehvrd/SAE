2d18e74ea25e8d7c12b89b41566044cf39b6f120
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-12-01 15:43:42 +0100

    re

--- index.php
+++ index.php
@@ -170,8 +170,8 @@
 
 
 
-/*
 
+$url='http://nominatim.openstreetmap.org/search?q=1645,%20route%20des%20Lucioles,%20Biot&format=json';
 $ch=curl_init($url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
 $response = curl_exec($ch);
@@ -189,41 +189,8 @@ var_dump($response);
                                 } else {
                                     // En cas d'erreur ou si aucune correspondance n'est trouvée, afficher un message
                                     echo "Erreur lors de l'extraction des données de géocodage.";
-                                }*/
+                                }
 
-                                // replace all the white space with "+" sign to match with google search pattern	
-// Adresse à géocoder
-$Adr_Uti_En_Cours = str_replace(" ", "+", $Adr_Uti_En_Cours);
-
-
-// URL pour cURL
-$url_curl = "http://nominatim.openstreetmap.org/search?q=1645,%20route%20des%20Lucioles,%20Biot&format=json";
-
-// Initialiser cURL
-$ch = curl_init($url_curl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-// Exécuter la requête cURL
-$response_curl = curl_exec($ch);
-
-// Vérifier s'il y a eu une erreur cURL
-if (curl_errno($ch)) {
-    echo 'Erreur cURL : ' . curl_error($ch);
-} else {
-    // Analyser la réponse JSON
-    $json_curl = json_decode($response_curl, true);
-
-    // Latitude et Longitude avec cURL
-    $latitude_curl = ($json_curl['results'][0]['geometry']['location']['lat']) ? $json_curl['results'][0]['geometry']['location']['lat'] : '--';
-    $longitude_curl = ($json_curl['results'][0]['geometry']['location']['lng']) ? $json_curl['results'][0]['geometry']['location']['lng'] : '--';
-
-    // Affichage avec cURL
-    echo "Latitude (cURL): $latitude_curl<br>";
-    echo "Longitude (cURL): $longitude_curl<br>";
-}
-
-// Fermer la ressource cURL
-curl_close($ch);
 
                                 if ($result->num_rows > 0) {
                                     while ($row = $result->fetch_assoc()) {
