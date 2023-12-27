685a1e5e9e8eeaa14cc20dc05fd60968b743a593
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-12-27 15:35:52 +0100

    xss sql insert commande

--- insert_commande.php
+++ insert_commande.php
@@ -10,9 +10,9 @@
       }
 
     session_start();
-    $Id_Uti=$_SESSION["Id_Uti"];
+    $Id_Uti=htmlspecialchars($_SESSION["Id_Uti"]);
     $Url = $_GET;
-    $Id_Prod=$Url["Id_Prod"];
+    $Id_Prod=htmlspecialchars($Url["Id_Prod"]);
     unset($Url["Id_Prod"]);
 
     $i=1;
@@ -21,16 +21,29 @@
     $queryNbCommandes = $bdd->query(('SELECT MAX(Id_Commande) FROM COMMANDE;'));
     $returnqueryNbCommandes = $queryNbCommandes->fetchAll(PDO::FETCH_ASSOC);
     $nbCommandes = $returnqueryNbCommandes[0]["MAX(Id_Commande)"]+1;
-    $insertionCommande = "INSERT INTO COMMANDE (Id_Commande, Id_Statut, Id_Prod, Id_Uti) VALUES ($nbCommandes, 1, $Id_Prod, $Id_Uti);";
 
-    $bdd->query($insertionCommande);
+    $insertionCommande = "INSERT INTO COMMANDE (Id_Commande, Id_Statut, Id_Prod, Id_Uti) VALUES (:nbCommandes, 1, :Id_Prod, :Id_Uti)";
+    $bindInsertionCommande = $bdd->prepare($insertionCommande);
+    $bindInsertionCommande->bindParam(':nbCommandes', $nbCommandes, PDO::PARAM_INT);
+    $bindInsertionCommande->bindParam(':Id_Prod', $Id_Prod, PDO::PARAM_INT);
+    $bindInsertionCommande->bindParam(':Id_Uti', $Id_Uti, PDO::PARAM_INT);
+    $bindInsertionCommande->execute();
 
     foreach ($Url as $produit => $quantite) {
       if ($quantite>0){        
-        $insertionProduit = "INSERT INTO CONTENU (Id_Commande, Id_Produit, Qte_Produit_Commande, Num_Produit_Commande) VALUES ($nbCommandes, $produit, $quantite, $i);";
-        $bdd->query($insertionProduit);
-        $updateProduit="UPDATE PRODUIT SET Qte_Produit = Qte_Produit-".$quantite." WHERE Id_Produit = ".$produit .";";
-        $bdd->exec($updateProduit);
+        $insertionProduit = "INSERT INTO CONTENU (Id_Commande, Id_Produit, Qte_Produit_Commande, Num_Produit_Commande) VALUES (:nbCommandes, :produit, :quantite, :i)";
+        $bindInsertionProduit = $bdd->prepare($insertionProduit);
+        $bindInsertionProduit->bindParam(':nbCommandes', $nbCommandes, PDO::PARAM_INT);
+        $bindInsertionProduit->bindParam(':produit', $produit, PDO::PARAM_INT);
+        $bindInsertionProduit->bindParam(':quantite', $quantite, PDO::PARAM_INT);
+        $bindInsertionProduit->bindParam(':i', $i, PDO::PARAM_INT);
+        $bindInsertionProduit->execute();
+
+        $updateProduit = "UPDATE PRODUIT SET Qte_Produit = Qte_Produit - :quantite WHERE Id_Produit = :produit";
+        $bindUpdateProduit = $bdd->prepare($updateProduit);
+        $bindUpdateProduit->bindParam(':quantite', $quantite, PDO::PARAM_INT);
+        $bindUpdateProduit->bindParam(':produit', $produit, PDO::PARAM_INT);
+        $bindUpdateProduit->execute();
         $i++;}
     }
     
