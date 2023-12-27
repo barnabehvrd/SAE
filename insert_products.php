e8b1cddd55d1bea28cba57424237e0cd6519cfc2
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-12-27 13:48:59 +0100

    xss sql insert products

--- insert_products.php
+++ insert_products.php
@@ -8,12 +8,17 @@
         }
 
     session_start();
-    $Id_Uti=$_SESSION["Id_Uti"];
+    $Id_Uti=htmlspecialchars($_SESSION["Id_Uti"]);
+
     $bdd=dbConnect();
     $queryNbProduits = $bdd->query(('SELECT MAX(Id_Produit) FROM PRODUIT;'));
     $returnqueryNbProduits = $queryNbProduits->fetchAll(PDO::FETCH_ASSOC);
     $nbProduits = $returnqueryNbProduits[0]["MAX(Id_Produit)"]+1;
-    $queryIdProd = $bdd->query(('SELECT Id_Prod FROM PRODUCTEUR WHERE Id_Uti='.$Id_Uti.';'));
+
+    $queryIdProd = $bdd->prepare('SELECT Id_Prod FROM PRODUCTEUR WHERE Id_Uti=:Id_Uti;');
+    $queryIdProd->bindParam(":Id_Uti", $Id_Uti, PDO::PARAM_STR);
+    $queryIdProd->execute();
+
     $returnQueryIdProd = $queryIdProd->fetchAll(PDO::FETCH_ASSOC);
     $IdProd = $returnQueryIdProd[0]["Id_Prod"];
     $Nom_Produit=$_POST["nomProduit"];
@@ -24,7 +29,7 @@
     $Unite_Quantite=$_POST["unitQuantite"];
     
     
-    $insertionProduit = "INSERT INTO PRODUIT (Id_Produit, Nom_Produit, Id_Type_Produit, Id_Prod, Qte_Produit, Id_Unite_Stock, Prix_Produit_Unitaire, Id_Unite_Prix) VALUES (".$nbProduits.", '".$Nom_Produit."', ".$Type_De_Produit.", ".$IdProd.", ".$Quantite.", ".$Unite_Quantite.", ".$Prix.", ".$Unite_Prix.");";
+    $insertionProduit = "INSERT INTO PRODUIT (Id_Produit, Nom_Produit, Id_Type_Produit, Id_Prod, Qte_Produit, Id_Unite_Stock, Prix_Produit_Unitaire, Id_Unite_Prix) VALUES (:nbProduits, :Nom_Produit, :Type_De_Produit, :IdProd, :Quantite, :Unite_Quantite, :Prix, :Unite_Prix)";
     echo $insertionProduit;
     //echo '<br>';
     //var_dump($_SESSION);
@@ -66,7 +71,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
     
 }
-    $_SESSION["Id_Produit"]=$nbProduits;
-    $bdd->query($insertionProduit);
+    $_SESSION["Id_Produit"]=htmlspecialchars($nbProduits);
+
+    $bindInsertProduct = $bdd->prepare($insertionProduit);
+    $bindInsertProduct->bindParam(':nbProduits', $nbProduits, PDO::PARAM_INT);
+    $bindInsertProduct->bindParam(':Nom_Produit', $Nom_Produit, PDO::PARAM_STR);
+    $bindInsertProduct->bindParam(':Type_De_Produit', $Type_De_Produit, PDO::PARAM_INT);
+    $bindInsertProduct->bindParam(':IdProd', $IdProd, PDO::PARAM_INT);
+    $bindInsertProduct->bindParam(':Quantite', $Quantite, PDO::PARAM_INT);
+    $bindInsertProduct->bindParam(':Unite_Quantite', $Unite_Quantite, PDO::PARAM_INT);
+    $bindInsertProduct->bindParam(':Prix', $Prix, PDO::PARAM_INT);
+    $bindInsertProduct->bindParam(':Unite_Prix', $Unite_Prix, PDO::PARAM_INT);
+    $bindInsertProduct->execute();
+
     header('Location: mes_produits.php');
 ?>
