6d1b5190afafc0f985ef7ae1a1069aa8192b257c
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2023-12-27 15:55:32 +0100

    xss sql traitement_formulaire_sign_in_admin

--- traitement_formulaire_sign_in_admin.php
+++ traitement_formulaire_sign_in_admin.php
@@ -1,11 +1,9 @@
 <?php
-
-
 // Error handling with try-catch block
 try {
     // Retrieve form data
-    $pwd = $_POST['pwd'];
-    $Mail_Uti = $_POST['mail'];
+    $pwd = htmlspecialchars($_POST['pwd']);
+    $Mail_Uti = htmlspecialchars($_POST['mail']);
 
     // Start session
     session_start();
@@ -25,7 +23,9 @@ try {
     $bdd = new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse);
 
     // Check if user email exists
-    $queryIdUti = $bdd->query('SELECT Id_Uti FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=\'' . $Mail_Uti . '\'');
+    $queryIdUti = $bdd->prepare('SELECT Id_Uti FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti = :userMail');
+    $queryIdUti->bindParam(':userMail', $Mail_Uti, PDO::PARAM_STR);
+    $queryIdUti->execute();
     $returnQueryIdUti = $queryIdUti->fetchAll(PDO::FETCH_ASSOC);
 
     // Handle invalid email
@@ -39,8 +39,11 @@ try {
     $Id_Uti = $returnQueryIdUti[0]["Id_Uti"];
     // Verify password using stored procedure
     //echo('CALL verifMotDePasse(' . $Id_Uti . ', \'' . $pwd . '\');');
-    $query = $bdd->query('CALL verifMotDePasse(' . $Id_Uti . ', \'' . $pwd . '\')');
 
+    $query = $bdd->prepare('CALL verifMotDePasse(:userId, :password)');
+    $query->bindParam(':userId', $Id_Uti, PDO::PARAM_INT);
+    $query->bindParam(':password', $pwd, PDO::PARAM_STR);
+    $query->execute();
     $test = $query->fetchAll(PDO::FETCH_ASSOC);
 
     // Handle password verification
@@ -48,7 +51,9 @@ try {
         if ($test[0][1] == 1 ) {
             //bon mdp
             $bdd3 = new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse);
-            $queryIdAdmin = $bdd3->query('SELECT Id_Uti FROM ADMINISTRATEUR WHERE ADMINISTRATEUR.Id_Uti=\'' . $Id_Uti . '\'');
+            $queryIdAdmin = $bdd3->prepare('SELECT Id_Uti FROM ADMINISTRATEUR WHERE ADMINISTRATEUR.Id_Uti = :idUti');
+            $queryIdAdmin->bindParam(':idUti', $Id_Uti, PDO::PARAM_STR);
+            $queryIdAdmin->execute();            
             $returnQueryIdAdmin = $queryIdUti->fetchAll(PDO::FETCH_ASSOC);
             $_SESSION['Mail_Uti'] = $Mail_Uti;
             $_SESSION['Id_Uti'] = $Id_Uti;
@@ -63,7 +68,7 @@ try {
             }
         } else {
             $_SESSION['test_pwd']--;
-            header('Location: form_sign_in_admin.php?pwd=mauvais mot de passe il vous restes ' . $_SESSION['test_pwd'] . ' tentative(s)');
+            header('Location: form_sign_in_admin.php?pwd=mauvais mot de passe il vous restes ' . htmlspecialchars($_SESSION['test_pwd']) . ' tentative(s)');
         }
     }else {
         header('Location: form_sign_in_admin.php?pwd=vous avez épuisé toutes vos tentatives de connection');
