94d41f9e2fdb4281969dc8e9e49e01931aa0e8b0
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2024-01-06 13:01:09 +0100

    test contenu

--- test/achats.php
+++ test/achats.php
@@ -45,7 +45,7 @@
             Statut 
             <br>
             
-            <form action="commandes.php" method="post">
+            <form action="achats.php" method="post">
                 <label>
                     <input type="radio" name="typeCategorie" value="0" <?php if($filtreCategorie==0) echo 'checked="true"';?>> TOUT
                 </label>
@@ -96,7 +96,84 @@
 
             <div class="contenuPage">
 
-               <!-- some code -->
+               
+            <?php
+				$query='SELECT Desc_Statut, Id_Commande, Nom_Uti, Prenom_Uti, Adr_Uti, COMMANDE.Id_Statut FROM COMMANDE INNER JOIN info_producteur ON COMMANDE.Id_Prod=info_producteur.Id_Prod INNER JOIN STATUT ON COMMANDE.Id_Statut=STATUT.Id_Statut WHERE COMMANDE.Id_Uti= :utilisateur';
+				if ($filtreCategorie!=0){
+					$query=$query.' AND COMMANDE.Id_Statut= :filtreCategorie ;';
+				}
+				$queryGetCommande = $bdd->prepare($query);
+				$queryGetCommande->bindParam(":utilisateur", $utilisateur, PDO::PARAM_STR);
+				if ($filtreCategorie!=0){
+					$queryGetCommande->bindParam(":filtreCategorie", $filtreCategorie, PDO::PARAM_STR);
+				}
+				$queryGetCommande->execute();
+                $returnQueryGetCommande = $queryGetCommande->fetchAll(PDO::FETCH_ASSOC);
+                $iterateurCommande=0;
+				if(count($returnQueryGetCommande)==0 and ($filtreCategorie==0)){
+                    echo "Aucune commande pour le moment";
+					?>
+					<br>
+					<input type="button" onclick="window.location.href='index.php'" value="Découvrez nos producteurs">
+					<?php
+                }
+                elseif(count($returnQueryGetCommande)==0){
+                    echo "Aucune commande ne correspond à ces critères";
+                }
+                else{
+                    while ($iterateurCommande<count($returnQueryGetCommande)){
+						$Id_Commande = $returnQueryGetCommande[$iterateurCommande]["Id_Commande"];
+						$Nom_Prod = $returnQueryGetCommande[$iterateurCommande]["Nom_Uti"];
+						$Nom_Prod = mb_strtoupper($Nom_Prod);
+						$Prenom_Prod = $returnQueryGetCommande[$iterateurCommande]["Prenom_Uti"];
+						$Adr_Uti = $returnQueryGetCommande[$iterateurCommande]["Adr_Uti"];
+						$Desc_Statut = $returnQueryGetCommande[$iterateurCommande]["Desc_Statut"];
+						$Desc_Statut = mb_strtoupper($Desc_Statut);
+						$Id_Statut = $returnQueryGetCommande[$iterateurCommande]["Id_Statut"];
+
+
+						$total=0;
+						$queryGetProduitCommande = $bdd->prepare('SELECT Nom_Produit, Qte_Produit_Commande, Prix_Produit_Unitaire, Nom_Unite_Prix FROM produits_commandes  WHERE Id_Commande = :Id_Commande;');
+						$queryGetProduitCommande->bindParam(":Id_Commande", $Id_Commande, PDO::PARAM_STR);
+						$queryGetProduitCommande->execute();
+						$returnQueryGetProduitCommande = $queryGetProduitCommande->fetchAll(PDO::FETCH_ASSOC);
+						$iterateurProduit=0;
+						$nbProduit=count($returnQueryGetProduitCommande);
+
+						if ($nbProduit>0){
+							echo '<div class="commande" >';
+							echo "Commande n°" . $iterateurCommande+1 ." : Chez ".$Prenom_Prod.' '.$Nom_Prod.' - '.$Adr_Uti;
+							echo '</br>';
+							echo $Desc_Statut;
+							echo '</br>';
+							if ($Id_Statut!=3 and $Id_Statut!=4){
+							echo '<form action="delete_commande.php" method="post">';
+							echo '<input type="hidden" name="deleteValeur" value="'.$Id_Commande.'">';
+							
+							echo '<button type="submit">Annuler commande</button>';
+							echo '</form>';
+							}
+						}
+
+						while ($iterateurProduit<$nbProduit){
+							$Nom_Produit=$returnQueryGetProduitCommande[$iterateurProduit]["Nom_Produit"];
+							$Qte_Produit_Commande=$returnQueryGetProduitCommande[$iterateurProduit]["Qte_Produit_Commande"];
+							$Nom_Unite_Prix=$returnQueryGetProduitCommande[$iterateurProduit]["Nom_Unite_Prix"];
+							$Prix_Produit_Unitaire=$returnQueryGetProduitCommande[$iterateurProduit]["Prix_Produit_Unitaire"];
+							echo "- " . $Nom_Produit ." - ".$Qte_Produit_Commande.' '.$Nom_Unite_Prix.' * '.$Prix_Produit_Unitaire.'€ = '.intval($Prix_Produit_Unitaire)*intval($Qte_Produit_Commande).'€';
+							echo "</br>";
+							$total=$total+intval($Prix_Produit_Unitaire)*intval($Qte_Produit_Commande);
+							$iterateurProduit++;
+						}
+                        $iterateurCommande++;
+						if ($nbProduit>0){
+							echo '<div class="aDroite">Total : '.$total.'€</div>';
+							echo '</div> '; 
+						}
+                    }
+                }
+            ?>
+
 
             </div>
             <div class="basDePage">
