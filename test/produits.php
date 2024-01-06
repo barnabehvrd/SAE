6bcf450eecaea70a34f19a351cadaede7d7ea337
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2024-01-06 19:33:55 +0100

    new taille

--- test/produits.php
+++ test/produits.php
@@ -157,7 +157,7 @@
                                         echo "Produit : " . $nomProduit . "<br>";
                                         echo "Type : " . $typeProduit . "<br>";
                                         // /test/  A SUPPRIMER QUAND ON REFERA LE VFINALE SUR SERVER !!!!!!
-                                        echo '<img class="img-produit" src="/~inf2pj02/test/img_produit/' . $Id_Produit  . '.png" alt="Image non fournie" style="width: 100%; height: 85%;" ><br>';
+                                        echo '<img class="img-produit" src="/~inf2pj02/test/img_produit/' . $Id_Produit  . '.png" alt="Image non fournie" style="width: 85%; height: 70%;" ><br>';
                                         echo "Prix : " . $prixProduit .' €/'.$unitePrixProduit. "<br>";
                                         echo "Stock : " . $QteProduit .' '.$Nom_Unite_Stock. "<br>";
                                         echo '<form action="product_modification.php" method="post">';
