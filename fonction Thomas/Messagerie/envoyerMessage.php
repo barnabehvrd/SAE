5640e19dcb3dca252cde4e2f20f762c1837351d6
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2024-01-07 11:47:16 +0100

    htmlspecialchars dans envoimessage

--- fonction Thomas/Messagerie/envoyerMessage.php
+++ fonction Thomas/Messagerie/envoyerMessage.php
@@ -4,7 +4,8 @@ ini_set("display_errors", 1);
 
 function envoyerMessage($id_user, $id_other_people, $content){
     $bdd = dbConnect();
-    $query = $bdd->query(('CALL envoyerMessage('.$id_user.', '.$id_other_people.", '".$content."');"));
+    
+    $query = $bdd->query(('CALL envoyerMessage('.$id_user.', '.$id_other_people.", '".htmlspecialchars($content)."');"));
     
     
 }
