cdc312f7048e610aeb085381339a32f968a2de64
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2024-01-08 20:44:10 +0100

    cgu

--- popups/pre_sign_up.php
+++ popups/pre_sign_up.php
@@ -1,3 +1,6 @@
+<?php
+    require "language.php" ; 
+?>
 <div class="popup">
     <div class="contenuPopup">
         <div>
@@ -5,16 +8,16 @@
                     <input type="submit" value="" class="boutonQuitPopup">
                     <input type="hidden" name="popup" value="">
             </form>
-            <p class="titrePopup">Choisir son profil</p>
+            <p class="titrePopup"><?php echo $htmlChoisirProfil; ?></p>
         </div>
         <div class="formPopup">
             <div class="alignementCentreCoteACote">
                 <form method="post">
-                        <input type="submit" value="Je suis un client" class="boutonPopup">
+                        <input type="submit" value="<?php echo $htmlJeSuisClient; ?>" class="boutonPopup">
                         <input type="hidden" name="popup" value="sign_up_client">
                 </form>
                 <form method="post">
-                        <input type="submit" value="Je suis un producteur" class="boutonPopup">
+                        <input type="submit" value="<?php echo $htmlJeSuisProducteur; ?>" class="boutonPopup">
                         <input type="hidden" name="popup" value="sign_up_prod">
                 </form>
             </div>
