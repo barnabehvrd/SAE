027bee14685c2d85902ad081be3308c6739eb98d
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2024-01-08 13:46:46 +0100

    fuifbezibfue bgyzebvgfzyeyez

--- achats.php
+++ achats.php
@@ -7,50 +7,74 @@
     <link rel="stylesheet" type="text/css" href="css/popup.css">
 </head>
 <body>
+
     <?php
-        if(!isset($_SESSION)){
-            session_start();
+    if(!isset($_SESSION)){
+        session_start();
         }
+
+        function dbConnect(){
+            $utilisateur = "inf2pj02";
+            $serveur = "localhost";
+            $motdepasse = "ahV4saerae";
+            $basededonnees = "inf2pj_02";
+            // Connect to database
+            return new PDO('mysql:host=' . $serveur . ';dbname=' . $basededonnees, $utilisateur, $motdepasse);
+        }
+
+        $bdd=dbConnect();
+        $utilisateur=htmlspecialchars($_SESSION["Id_Uti"]);
+        
+        $filtreCategorie=0;
+        if (isset($_POST["typeCategorie"])==true){
+            $filtreCategorie=htmlspecialchars($_POST["typeCategorie"]);
+        }
+    
     ?>
+
     <div class="container">
         <div class="leftColumn">
 			<img class="logo" href="index.php" src="img/logo.png">
             <div class="contenuBarre">
-                <!-- some code -->
+                
+            
+            <center>
+                <p><strong>Filtrer par :</strong></p>
+                <br>
+            </center>
+            Statut 
+            <br>
+            
+            <form action="achats.php" method="post">
+                <label>
+                    <input type="radio" name="typeCategorie" value="0" <?php if($filtreCategorie==0) echo 'checked="true"';?>> TOUT
+                </label>
+                <br>
+                <label>
+                    <input type="radio" name="typeCategorie" value="1" <?php if($filtreCategorie==1) echo 'checked="true"';?>> EN COURS
+                </label>
+                <br>
+                <label>
+                    <input type="radio" name="typeCategorie" value="2"<?php if($filtreCategorie==2) echo 'checked="true"';?>> PRÊTE
+                </label>
+                <br>
+                <label>
+                    <input type="radio" name="typeCategorie" value="4" <?php if($filtreCategorie==4) echo 'checked="true"';?>> LIVRÉE
+                </label>
+                <br>
+                <label>
+                    <input type="radio" name="typeCategorie" value="3" <?php if($filtreCategorie==3) echo 'checked="true"';?>> ANNULÉE
+                </label>
+
+                <br>
+                <br>
+                <center>
+                    <input type="submit" value="Filtrer">
+                </center>
+            </form>
+
             </div>
         </div>
-        <div class="rightColumn">
-            <div class="topBanner">
-                <div class="divNavigation">
-                    <a class="bontonDeNavigation" href="index.php">Accueil</a>
-                    <?php
-                        if (isset($_SESSION["Id_Uti"])){
-                            echo'<a class="bontonDeNavigation" href="messagerie.php">Messagerie</a>';
-                            echo'<a class="bontonDeNavigation" href="achats.php">Achats</a>';
-                        }
-                        if (isset($_SESSION["isProd"]) and ($_SESSION["isProd"]==true)){
-                            echo'<a class="bontonDeNavigation" href="produits.php">Produits</a>';
-                            echo'<a class="bontonDeNavigation" href="delivery.php">Commandes</a>';
-                        }
-                        if (isset($_SESSION["isAdmin"]) and ($_SESSION["isAdmin"]==true)){
-                            echo'<a class="bontonDeNavigation" href="panel_admin.php">Panel Admin</a>';
-                        }
-                    ?>
-                </div>
-                <form method="post">
-                    <?php
-                    if(!isset($_SESSION)){
-                    session_start();
-                    }
-                    if(isset($_SESSION, $_SESSION['tempPopup'])){
-                        $_POST['popup'] = $_SESSION['tempPopup'];
-                        unset($_SESSION['tempPopup']);
-                    }
-                    ?>
-					<input type="submit" value=<?php if (!isset($_SESSION['Mail_Uti'])){/*$_SESSION = array()*/; echo '"Se Connecter"';}else {echo '"'.$_SESSION['Mail_Uti'].'"';}?> class="boutonDeConnection">
-                    <input type="hidden" name="popup" value=<?php if(isset($_SESSION['Mail_Uti'])){echo '"info_perso"';}else{echo '"sign_in"';}?>>
-				</form>
-            </div>
         <div class="rightColumn">
             <div class="topBanner">
                 <div class="divNavigation">
