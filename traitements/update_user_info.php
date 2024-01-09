9571031b258250ca194a06ae521a621407a47989
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2024-01-09 18:01:50 +0100

    Update update_user_info.php

--- traitements/update_user_info.php
+++ traitements/update_user_info.php
@@ -13,14 +13,14 @@ if (isset($_POST['new_nom'], $_POST['new_prenom'], $_POST['rue'], $_POST['code']
         session_start();
     }
 
-    $update="UPDATE UTILISATEUR SET Nom_Uti = '".$_POST["new_nom"]."',". "Prenom_Uti = '".$_POST["new_prenom"]."',". "Adr_Uti = '".$adr."' WHERE Mail_Uti = '".$_SESSION["Mail_Uti"] ."';";
+    $update="UPDATE UTILISATEUR SET Nom_Uti = '".$_POST["new_nom"]."',". "Prenom_Uti = '".$_POST["new_prenom"]."',". "Adr_Uti = '".$adr."'". "Pwd_Uti = '".$_POST['pwd']."' WHERE Mail_Uti = '".$_SESSION["Mail_Uti"] ."';";
 
     echo ($update);
     $bdd->exec($update);
     
-    header('Location: ../index.php');  
+   // header('Location: ../index.php');  
 }else{
-    header('Location: ../index.php');    
+    // header('Location: ../index.php');    
 
 }
 ?>
