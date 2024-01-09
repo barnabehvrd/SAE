1809440c528462f7f03ca4d8ead104eba12c877b
KillianLeHenaff <129835835+KillianLeHenaff@users.noreply.github.com>
2024-01-09 16:49:06 +0100

    corr

--- index.php
+++ index.php
@@ -228,7 +228,7 @@
                 </div>
                 <form action="language.php" method="post" id="languageForm">
                     <select name="language" id="languagePicker" onchange="submitForm()">
-                        <option value="fr" <?php if($_SESSION["language"]=="fr") echo 'selected';?>>Français</option>
+                        <option value="fr" <?php if(!(isset($_SESSION["language"]))OR($_SESSION["language"]=="fr")) echo 'selected';?>>Français</option>
                         <option value="en" <?php if($_SESSION["language"]=="en") echo 'selected';?>>English</option>
                         <option value="es" <?php if($_SESSION["language"]=="es") echo 'selected';?>>Español</option>
                         <option value="al" <?php if($_SESSION["language"]=="al") echo 'selected';?>>Deutsch</option>
