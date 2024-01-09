f872d33b8d435c2d072424c52926e7f06f6d4cbc
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2024-01-09 11:35:48 +0100

    grenade

--- language.php
+++ language.php
@@ -3,42 +3,33 @@
  
 if (isset($_POST['language'])) {
     $_SESSION["language"] = $_POST['language'];
+    header("Location: index.php");
+
 } 
     if (isset($_SESSION["language"])) {
     switch ($_SESSION["language"]) {
         case 'fr':
             require "language_fr.php" ;
-            header("Location: index.php");
- 
             break;
 
         case 'en':
             require "language_en.php" ;
-            header("Location: index.php");
-          
             break;
 
         case 'es':
             require "language_es.php" ;
-            header("Location: index.php");
-    
             break;
 
         case 'de':
             require "language_al.php" ;
-            header("Location: index.php");
-
             break;
 
         default:
         require "language_fr.php" ;
-        header("Location: index.php");
-
             break;
         }
     }else {
         require "language_fr.php" ;
-        header("Location: index.php");
  
     }
 
