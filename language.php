892102844059956e61424d0c68763523fd3810cc
JDjudgedead <88926501+JDjudgedead4@users.noreply.github.com>
2024-01-09 11:10:54 +0100

    yolo

--- language.php
+++ language.php
@@ -1,3 +1,17 @@
 <?php
-    require "language_en.php" ; 
+    require "language_fr.php" ; 
+    echo ('
+
+        <form action="language.php" method="post">
+        <label for="languagePicker">Choose a language:</label>
+        <select name="language" id="languagePicker">
+            <option value="fr">Français</option>
+            <option value="en">English</option>
+            <option value="es">Español</option>
+            <option value="de">Deutsch</option>
+        </select>
+        <input type="submit" value="Submit">
+    </form>
+
+    ')
 ?>
