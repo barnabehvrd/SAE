c840c733e85e116c76e4ef37913d1cdd8e075bba
v0ltis <voltis.discord@gmail.com>
2025-03-18 15:31:18 +0100

    Add proxy settings for cURL requests in latLongGps function

--- index.php
+++ index.php
@@ -73,6 +73,10 @@ ini_set('display_errors', 1);
         function latLongGps($url){
             // Configuration de la requête cURL
             $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_PROXY, 'proxy.univ-lemans.fr');
+            curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+            curl_setopt($ch, CURLOPT_HTTPPROXYTUNNEL, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
             curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); // Permet de suivre les redirections
             // Ajout du User Agent
