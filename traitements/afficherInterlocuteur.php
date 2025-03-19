7f1c922c09499b56e8f3e31f08af359a3719881b
v0ltis <voltis.discord@gmail.com>
2025-03-19 18:02:46 +0100

    Fix user retrieval and result display in afficherInterlocuteur.php

--- traitements/afficherInterlocuteur.php
+++ traitements/afficherInterlocuteur.php
@@ -7,9 +7,11 @@ $db = new database();
 
 if (isset($_GET['Id_Interlocuteur'])){
 
-    $interlocuteur = $db->select('CALL isProducteur(:Id_Interlocuteur);', array('Id_Interlocuteur' => $_GET['Id_Interlocuteur']));
+    $interlocuteur = $db->select('SELECT Nom_Uti, Prenom_Uti FROM UTILISATEUR WHERE Id_Uti= :Id_Interlocuteur;', array('Id_Interlocuteur' => $_GET['Id_Interlocuteur']));
 
-    echo ($interlocuteur[0]['Nom_Uti'].' '.$interlocuteur[0]['Prenom_Uti'].($interlocuteur[0]['result']));
+    $result = $db->select('CALL isProducteur(:Id_Interlocuteur);', array('Id_Interlocuteur' => $_GET['Id_Interlocuteur']));
+
+    echo ($interlocuteur[0]['Nom_Uti'].' '.$interlocuteur[0]['Prenom_Uti'].($result[0]['result']));
 
 }
 
