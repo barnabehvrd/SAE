e4928ac42ba14f8d98e4b4f9f35ba9c1cba1b887
v0ltis <voltis.discord@gmail.com>
2025-03-19 18:07:04 +0100

    Fix user retrieval and result display in afficherInterlocuteur.php

--- traitements/chargement_info_perso.php
+++ traitements/chargement_info_perso.php
@@ -5,9 +5,6 @@ use database\database;
 
 $db = new database();
 
-// Préparez la requête SQL en utilisant des requêtes préparées pour des raisons de sécurité
-$requete = 'SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=?';
-
 
 $result = $db->select('SELECT * FROM UTILISATEUR WHERE UTILISATEUR.Mail_Uti=:Mail_Uti', array('Mail_Uti' => $_SESSION['Mail_Uti']));
 ?>
