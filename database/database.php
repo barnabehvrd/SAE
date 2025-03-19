e499fef4b4ae28bc32d7579f85958ffc5680f12d
v0ltis <voltis.discord@gmail.com>
2025-03-19 14:21:08 +0100

    C'EST FINI !!!!

--- database/database.php
+++ database/database.php
@@ -45,8 +45,10 @@ class database
         foreach ($params as $key => &$val) {
             $htmlspecialchars = htmlspecialchars($val);
             if (is_int($val)) {
+                echo $key . ' is int : ' . $val . "\n";
                 $stmt->bindParam($key, $htmlspecialchars, PDO::PARAM_INT);
             } else {
+                echo $key . ' is string : ' . $val . "\n";
                 $stmt->bindParam($key, $htmlspecialchars, PDO::PARAM_STR);
             }
         }
