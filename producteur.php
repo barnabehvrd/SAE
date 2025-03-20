456bc7f483d9b529fa6cdec0ece07592ee322c86
Gael <gaelquem44@gmail.com>
2025-03-20 16:06:53 +0100

    dsfsqdf

--- producteur.php
+++ producteur.php
@@ -163,7 +163,7 @@
                                 <?php if (sizeof($returnQueryGetProducts)>0 and isset($_SESSION["Id_Uti"]) and $idUti!=$_SESSION["Id_Uti"]): ?>
                                     <input type="button" class="btn btn-outline-success" onclick="window.location.href='messagerie.php?Id_Interlocuteur=<?php echo $idUti ?>'" value="<?php echo $htmlEnvoyerMessage; ?>">
                                     <input type="hidden" name="Id_Prod" value="<?php echo $idUti ?>">
-                                    <buitton type="submit" class="btn btn-success w-100"><?php echo $htmlPasserCommande; ?></button>
+                                    <button type="submit"><?php echo $htmlPasserCommande; ?></button>
                                     
                                 <?php endif; ?>
                             <?php endif; ?>
@@ -201,10 +201,9 @@
                         
                     </div>
                  </div>
-                 </form>
+            </form>     
             </div>
         </div>
-        
     </div>
     </main>
     <footer class="bg-light d-flex justify-content-center align-items-center">
