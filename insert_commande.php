9aee4b060afaee7da50cf9130deb09863217392c
Louison Petit <louisonpetit67@gmail.com>
2025-03-20 16:00:52 +0100

    Correction erreurs due au merge

--- insert_commande.php
+++ insert_commande.php
@@ -1,42 +1,31 @@
 <?php
 
-
 require_once 'database/database.php';
 use database\database;
 
 $db = new database();
 
-  if(!isset($_SESSION)){
-    session_start();
-    }
-
-    $Id_Uti=$_SESSION["Id_Uti"];
-    $Url = $_GET;
-    $Id_Prod=$Url["Id_Prod"];
-    unset($Url["Id_Prod"]);
-
-    // Connect to database
-}
 if(!isset($_SESSION)){
     session_start();
 }
-$Id_Uti=htmlspecialchars($_SESSION["Id_Uti"]);
+
+$Id_Uti = htmlspecialchars($_SESSION["Id_Uti"]);
 $Url = $_GET;
-$Id_Prod=htmlspecialchars($Url["Id_Prod"]);
+$Id_Prod = htmlspecialchars($Url["Id_Prod"]);
 unset($Url["Id_Prod"]);
 
+$nbCommandes = $db->select('SELECT MAX(Id_Commande) FROM COMMANDE;')[0]["MAX(Id_Commande)"] + 1;
 
-    $nbCommandes = $db->select('SELECT MAX(Id_Commande) FROM COMMANDE;')[0]["MAX(Id_Commande)"]+1;
+$bindInsertionCommande = $db->query('INSERT INTO COMMANDE (Id_Commande, Id_Statut, Id_Prod, Id_Uti) VALUES (:nbCommandes, 1, :Id_Prod, :Id_Uti)', [
+    'nbCommandes' => $nbCommandes,
+    'Id_Prod' => $Id_Prod,
+    'Id_Uti' => $Id_Uti
+]);
 
-    $bindInsertionCommande = $db->query('INSERT INTO COMMANDE (Id_Commande, Id_Statut, Id_Prod, Id_Uti) VALUES (:nbCommandes, 1, :Id_Prod, :Id_Uti)', [
-        'nbCommandes' => $nbCommandes,
-        'Id_Prod' => $Id_Prod,
-        'Id_Uti' => $Id_Uti
-    ]);
+$i = 1; // Initialisation de la variable $i
 
-    foreach ($Url as $produit => $quantite) {
-      if ($quantite>0){
-
+foreach ($Url as $produit => $quantite) {
+    if ($quantite > 0) {
         $bindInsertionProduit = $db->query('INSERT INTO CONTENU (Id_Commande, Id_Produit, Qte_Produit_Commande, Num_Produit_Commande) VALUES (:nbCommandes, :produit, :quantite, :i)', [
             'nbCommandes' => $nbCommandes,
             'produit' => $produit,
@@ -49,8 +38,9 @@ unset($Url["Id_Prod"]);
             'produit' => $produit
         ]);
 
-
-      }
+        $i++; // Incrémentation de la variable $i
     }
+}
+
 header('Location: achats.php');
 ?>
--- language.php
+++ language.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if(!isset($_SESSION)){
+    session_start();
+}
 
 $languageFiles = [
     'en' => "language_en.php",
